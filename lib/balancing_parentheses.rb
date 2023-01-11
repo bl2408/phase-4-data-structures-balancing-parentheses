c27a5648b621eeaa343e309f4704e88ec1c47e39
Brian Lambert <brianlambert2408@gmail.com>
2023-01-11 13:28:48 +1100

    Completed tasks

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses str
+
+    stack = Stack.new
+
+    str.chars.each do | i |
+        if stack.peek == '(' && i ==")"
+            stack.pop
+        else
+            stack.push(i)
+        end
+    end
+    stack.size
+end
+
+# pp balancing_parentheses("(()()()))))")
